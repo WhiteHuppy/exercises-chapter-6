4bc8481a8c9d0a581d7d9894aa9a1cd396a908a9
Nikhil Ramchandran <nikhil.ramchandran18@imperial.ac.uk>
2022-02-17 21:21:13 +0000

    Implemented iterative methods.

--- nonlinear_solvers/solvers.py
+++ nonlinear_solvers/solvers.py
@@ -31,8 +31,14 @@ def newton_raphson(f, df, x_0, eps=1.0e-5, max_its=20):
     float
         The approximate root computed using Newton iteration.
     """
-    # Delete these two lines when implementing the method.
-    raise NotImplementedError
+
+    for _ in range(max_its):
+        x_0 -= f(x_0) / df(x_0)
+        if abs(f(x_0)) < eps:
+            return x_0
+
+    raise ConvergenceError("Newton-Raphson failed to converge after",
+                           f"{max_its} attempts.")
 
 
 def bisection(f, x_0, x_1, eps=1.0e-5, max_its=20):
@@ -60,8 +66,22 @@ def bisection(f, x_0, x_1, eps=1.0e-5, max_its=20):
     float
         The approximate root computed using bisection.
     """
-    # Delete these two lines when implementing the method.
-    raise NotImplementedError
+
+    if (f(x_0) < 0) == (f(x_1) < 0):
+        raise ValueError(f"starting points {x_0} and {x_1}",
+                         "have the same sign on f.")
+
+    for _ in range(max_its):
+        x_2 = (x_0 + x_1) / 2
+        if abs(f(x_2)) < eps:
+            return x_2
+        elif (f(x_2) < 0) == (f(x_0) < 0):
+            x_0 = x_2
+        else:
+            x_1 = x_2
+
+    raise ConvergenceError("bisection failed to converge after",
+                           f"{max_its} attempts.")
 
 
 def solve(f, df, x_0, x_1, eps=1.0e-5, max_its_n=20, max_its_b=20):
@@ -95,5 +115,7 @@ def solve(f, df, x_0, x_1, eps=1.0e-5, max_its_n=20, max_its_b=20):
     float
         The approximate root.
     """
-    # Delete these two lines when implementing the method.
-    raise NotImplementedError
+    try:
+        return newton_raphson(f, df, x_0, eps, max_its_n)
+    except ConvergenceError:
+        return bisection(f, x_0, x_1, eps, max_its_b)
